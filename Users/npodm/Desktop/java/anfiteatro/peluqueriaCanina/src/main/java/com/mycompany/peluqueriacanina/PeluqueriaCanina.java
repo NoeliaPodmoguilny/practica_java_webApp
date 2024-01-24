33db3646335ebac569ca8bc56fe913af25fc4884
NoePodmoguilny <npodmoguilny@gmail.com>
2024-01-24 17:11:04 -0300

    peluqueria canina v 1.0

---
+++ Users/npodm/Desktop/java/anfiteatro/peluqueriaCanina/src/main/java/com/mycompany/peluqueriacanina/PeluqueriaCanina.java
@@ -0,0 +1,13 @@
+package com.mycompany.peluqueriacanina;
+
+import com.mycompany.peluqueriacanina.igu.Principal;
+
+public class PeluqueriaCanina {
+
+    public static void main(String[] args) {
+        
+        Principal view = new Principal();
+        view.setVisible(true);
+        view.setLocationRelativeTo(null);
+    }
+}

cf76568449325701c15e3730018fd39929959081
NoePodmoguilny <npodmoguilny@gmail.com>
2024-01-27 12:56:04 -0300

    concesionaria v 1.0

---
+++ Users/npodm/Desktop/java/anfiteatro/concesionaria/src/main/java/com/mycompany/concesionaria/Concesionaria.java
@@ -0,0 +1,14 @@
+package com.mycompany.concesionaria;
+
+
+import com.mycompany.concesionaria.igu.PrincipalView;
+
+public class Concesionaria {
+
+    public static void main(String[] args) {
+        
+        PrincipalView view = new PrincipalView();
+        view.setVisible(true);
+        view.setLocationRelativeTo(null);
+    }
+}

59b59262a66575683eb2f4a82bec6d96f64179e5
NoePodmoguilny <npodmoguilny@gmail.com>
2024-02-01 20:35:27 -0300

    Login usuarios v 1.0

---
+++ Users/npodm/Desktop/javaLogin/login/src/main/java/com/mycompany/login/Login.java
@@ -0,0 +1,13 @@
+package com.mycompany.login;
+
+import com.mycompany.login.igu.LoginInicial;
+
+public class Login {
+
+    public static void main(String[] args) {
+
+        LoginInicial viewPpal = new LoginInicial();
+        viewPpal.setVisible(true);
+        viewPpal.setLocationRelativeTo(null);
+    }
+}
